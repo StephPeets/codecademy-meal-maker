c1c807ce0ea78c8b9b1c855082ff7ccff3b59ddc
Steph Peets <stephanie.peets@gmail.com>
2021-09-05 01:03:46 -0400

    complete steps 1-10

--- app.js
+++ app.js
@@ -11,12 +11,6 @@ let menu = {
       desserts: this._courses.desserts
     }
   },
-  // set _courses(course) {
-  //   return {
-  //     appetizers: this.appetizers.push(course),
-  //     mains: this.mains,
-  //     desserts: this.desserts
-  //   }
   get appetizers() {
     return this.courses.appetizers;
   },
@@ -27,32 +21,38 @@ let menu = {
     return this.courses.desserts;
   },
   set appetizers(appIn) {
-    return appetizers = this.appetizers.push(appIn);
+    return appetizers = this.appetizers.push({name: appIn[0], price: appIn[1]});
   },
   set mains(mainIn) {
-    return mains = this.mains.push(mainIn);
+    return mains = this.mains.push({name: mainIn[0], price: mainIn[1]});
   },
   set desserts(dessertIn) {
-    return desserts = this.desserts.push(dessertIn);
+    return desserts = this.desserts.push({name: dessertIn[0], price: dessertIn[1]});
   },
   addDishToCourse(courseName, dishName, dishPrice) {
     const dish = {
       name: dishName,
       price: dishPrice
     };
-    return menu[courseName] = dish;
+    return menu._courses[courseName].push(dish);
+  },
+  getRandomDishFromCourse(courseName){
+    let dishes = menu._courses[courseName];
+    let dishNum = (Math.random()*dishes.length);
+    let dish = Math.floor(dishNum);
+    return console.log(dishes[dish]);
+  },
+  generateRandomMeal() {
+    
+
   }
 }
-
-// console.log(menu.addDishToCourse(desserts, "food", 35));
-//  menu._courses.appetizers.push("food");
-menu.addDishToCourse("appetizers", "moreFood", "$12")
-menu.addDishToCourse("desserts", "moredessert", "$80")
-menu.addDishToCourse("desserts", "tiramoredessert", "$5000")
-console.log(menu._courses)
-
-
-
-
-
-
+menu.appetizers = ["a", "b"];
+menu.appetizers = ["food", "2"];
+menu.appetizers = ["lard", 12];
+// menu.appetizers = "13 slimjim"
+menu.mains = ["fish", "$1_000"];
+menu.mains = ["fishy chicken", "$10"];
+menu.addDishToCourse("desserts", "sweet butts", "if you have to ask...");
+menu.addDishToCourse("desserts", "swxgdxgs", "i575452.");
+menu.getRandomDishFromCourse("appetizers")
